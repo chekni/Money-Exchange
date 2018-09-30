48e3a4d47c28852308b5cb13c67cd6f77d785bb4
Karina <karina_cheshko@mail.ru>
2018-09-30 20:39:47 +0300

    split less money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+     if (currency <= 0)
+          return {};
+
+     if (currency >= 10000)
+          return { error: "You are rich, my friend! We don't have so much coins for exchange" };
+
+     let changeMoney = {};
+     changeMoney.H = 0;
+     changeMoney.Q = 0;
+     changeMoney.D = 0;
+     changeMoney.N = 0;
+     changeMoney.P = 0;
+
+     let tmp;
+
+     while (currency > 0) {
+
+          tmp = Math.trunc(currency / 50);
+          if (tmp !== 0) {
+               currency = currency % 50;
+               changeMoney.H = changeMoney.H + tmp;
+          }
+
+          tmp = Math.trunc(currency / 25);
+          if (tmp !== 0) {
+               currency = currency % 25;
+               changeMoney.Q = changeMoney.Q + tmp;
+          }
+
+          tmp = Math.trunc(currency / 10);
+          if (tmp !== 0) {
+               currency = currency % 10;
+               changeMoney.D = changeMoney.D + tmp;
+          }
+
+          tmp = Math.trunc(currency / 5);
+          if (tmp !== 0) {
+               currency = currency % 5;
+               changeMoney.N = changeMoney.N + tmp;
+          }
+
+          if (currency !== 0) {
+               changeMoney.P = currency;
+          }
+     }
+
+     let solution = {};
+
+     for (let key in changeMoney) {
+          if (changeMoney[key] !== 0)
+               solution[key] = changeMoney[key];
+     }
+
+     return solution;
 }
